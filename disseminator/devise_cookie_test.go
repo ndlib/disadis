12f18c4bbf656fb3d23c24197fabd76266e7f881
Don Brower <dbrower@nd.edu>
2014-02-17 12:13:00 -0500

    Move devise user lookup into interface. Add tests.

--- disseminator/devise_cookie_test.go
+++ disseminator/devise_cookie_test.go
@@ -1,15 +1,16 @@
 package disseminator
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
 
 func TestDevise(t *testing.T) {
-
 	var d = &DeviseAuth{
-		SecretBase: []byte("1597e868fa2dfb2f782fd4477d0e93d4f24c31f2da5d54edac15391eaf60dcd6aeba91856bc12fefc9d9ad329ab023ed1503cf8c51dc88e4ae4040655a92ae8f"),
+		SecretBase: []byte("0123456789abcdefghijklmnopqrstuvwxyz"),
 		CookieName: "_test_session",
+		lookup:     &NoLookup{},
 	}
 	req, err := http.NewRequest("GET", "http://example.com", nil)
 	if err != nil {
@@ -18,10 +19,39 @@ func TestDevise(t *testing.T) {
 	}
 	cookie := http.Cookie{
 		Name:  "_test_session",
-		Value: `BAh7DEkiD3Nlc3Npb25faWQGOgZFVEkiJWZiN2Q5NGIzZWU2OWVlOWFhNzVlYzU3Y2M1ZTlkZjdlBjsAVEkiC3NlYXJjaAY7AEZ7AEkiDGhpc3RvcnkGOwBGWwBJIhpjYXNfbGFzdF92YWxpZF90aWNrZXQGOwBUSSImU1QtOTQxNTItSnJoYTQxZWNoTzlLNUdqZUcwOXQtY2FzBjsAVEkiIGNhc19sYXN0X3ZhbGlkX3RpY2tldF9zdG9yZQY7AFRGSSIZd2FyZGVuLnVzZXIudXNlci5rZXkGOwBUWwdbBmkKMEkiEF9jc3JmX3Rva2VuBjsARkkiMVY3N1FFTTBKMmlVR3NZQk1yaTkrd1hPeTFrU1l4eTBXMUxRYm9hbjlvdlk9BjsARg==--1326c120c741e0de455aa638e6b8062c0edce25f`,
+		Value: `BAh7DEkiD3Nlc3Npb25faWQGOgZFVEkiJTU1YWE3OWU1MTE2ZTkyMzM4ZWMxYmJlNzNkMjlkM2Q5BjsAVEkiC3NlYXJjaAY7AEZ7AEkiDGhpc3RvcnkGOwBGWwBJIhpjYXNfbGFzdF92YWxpZF90aWNrZXQGOwBUSSIiU1QtMTg4MTgtbFNQckZ0ZFQ5WW1GUVA2aWM1dE4GOwBUSSIgY2FzX2xhc3RfdmFsaWRfdGlja2V0X3N0b3JlBjsAVEZJIhl3YXJkZW4udXNlci51c2VyLmtleQY7AFRbB1sGaQYwSSIQX2NzcmZfdG9rZW4GOwBGSSIxR3RWZ2hocG5pZjhqWUVPVDAveTlXTzZqUS8vZmJBK2pjREtjT2tWUTlDZz0GOwBG--2dd592ed4b4e7384c2febd20b0c4a3ff3629231c`,
 	}
 	req.AddCookie(&cookie)
 	u := d.User(req)
-	t.Errorf("%s", u.Id)
+	if u.Id != "User-1" {
+		t.Errorf("Got user %s", u.Id)
+	}
+}
 
+type NoLookup struct{}
+
+func (n *NoLookup) Lookup(uid int) (User, error) {
+	return User{Id: fmt.Sprintf("User-%d", uid)}, nil
+}
+
+func TestUnmarshal(t *testing.T) {
+	table := []struct {
+		v string
+		n int
+	}{
+		// These are the raw marshal values. They are not base64 encoded.
+		{v: "\x04\b{\bI\"\x0Fsession_id\x06:\x06ETI\"%491b3f98671c8c5cd6409c4371092efe\x06;\x00TI\"\x19warden.user.user.key\x06;\x00T[\a[\x06i\x060I\"\x10_csrf_token\x06;\x00FI\"\bZZZ\x06;\x00T",
+			n: 1},
+		{v: "\x04\b{\bI\"\x0Fsession_id\x06:\x06ETI\"%491b3f98671c8c5cd6409c4371092efe\x06;\x00TI\"\x19warden.user.user.key\x06;\x00T[\a[\x06i\x02\xDB\x030I\"\x10_csrf_token\x06;\x00FI\"\bZZZ\x06;\x00T",
+			n: 987},
+	}
+
+	for i, item := range table {
+		t.Logf("v = %s", item.v)
+		n, err := unmarshalDevise([]byte(item.v))
+		if n != item.n {
+			t.Errorf("Ticket %d returned wrong user id (%d instead of %d)", i, n, item.n)
+			t.Error(err)
+		}
+	}
 }
